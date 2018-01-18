82b093b0669b0d720df344aa28e3dc354fb5246f
autumnlike <akiyoshi.satoshi@lancers.co.jp>
2018-01-18 09:35:54 +0900

    不要だったjob_idを除外

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -1,12 +1,12 @@
 class CandidatesController < ApplicationController
   def index
     @candidates = Candidate.where(
-      job_id: [10, 11, 12, 18, 28, 39, 86],
+      job_id: [10, 11, 12, 18, 39, 86],
       status: [ '1次選考', '2次選考', '書類選考', '最終選考', '未対応', '内定']
     ).order(:entry_date)
   end
 
   def summary
-    @summary = Candidate.summary [10, 11, 12, 18, 28, 39, 86]
+    @summary = Candidate.summary [10, 11, 12, 18, 39, 86]
   end
 end
